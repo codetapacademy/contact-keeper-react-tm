9e6d7c1c7c6986bc1f361d22fa0b6f01687ea269
Bogdan Negura <id.bogdann@yahoo.com>
2019-10-14 11:27:44 +0100

    Delete Contact Form UI  implementation

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import PropTypes from 'prop-types'
+import ContactContext from '../../context/contact/contactContext'
 
 const ContactItem = ({ contact }) => {
+    const contactContext = useContext(ContactContext)
+    const { deleteContact } = contactContext
+
     const { id, name, email, phone, type } = contact
+
+    const onDelete = () => {
+        deleteContact(id)
+    }
+
     return (
         <div className="card bg-light">
             <h3 className="text-primary text-left">
-                {name}{' '} <span style={{ float: 'right' }} className={'badge' + (type === 'professional' ? 'badge-success' : 'badge-primary')}>{type.charAt(0).toUpperCase() + type.slice(1)}</span>
+                {name}{' '} 
+                <span style={{ float: 'right' }} className={'badge' + (type === 'professional' ? 'badge-success' : 'badge-primary')}>
+                    {type.charAt(0).toUpperCase() + type.slice(1)}
+                </span>
             </h3>
             <ul className="list">
                 {email && (<li>
@@ -17,7 +29,7 @@ const ContactItem = ({ contact }) => {
                 </li>)}
             </ul>
             <button className="btn btn-dark btn-sm">Edit</button>
-            <button className="btn btn-danger btn-sm">Delete</button>
+            <button className="btn btn-danger btn-sm" onClick={onDelete}>Delete</button>
         </div>
     )
 }
